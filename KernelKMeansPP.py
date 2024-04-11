84e40f64c2561824e529c12fc154d2fcf2bfdfee
JohnPapakostas <84929430+JohnPapakostas@users.noreply.github.com>
2024-04-11 15:26:17 +0300

    Add in Common_Modules Evaluation.py
    Print Metrics Reults in KernelKMeans.py and .ipynb

--- KernelKMeansPP.py
+++ KernelKMeansPP.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import math
 from InitialPartition import InitialPartition
+from Common_Modules.Evaluation import Evaluator
 
 class KernelKMeansPP:
 
     def __init__(self):
         self.initialPartition = InitialPartition()
+        self.evaluator = Evaluator()
 
     def find_cluster_indices(self, array, value):
         return np.where(np.array(array) == value)[0]
 
-    def custom_kernel_kmeans(self, X, clusters_labels, initial_partition, kernel_matrix, max_iter=300, tol=1e-4):
+    def custom_kernel_kmeans(self, X, y, clusters_labels, initial_partition, kernel_matrix, max_iter=300, tol=1e-4):
         n_samples = X.shape[0]
         n_clusters = len(clusters_labels)
         distances = np.zeros((n_clusters,n_samples))
@@ -39,25 +41,27 @@ class KernelKMeansPP:
             
             min_distances = np.min(distances, axis=0)
             total_error = np.sum(min_distances)
-            #print(f"\n Total Error is: {total_error}!")
             
             next_partition = np.argmin(distances, axis=0)
             are_equal = np.array_equal(initial_partition, next_partition)
             if(are_equal):
-                print(f"\n Finished in {iter} iterations!")
-                print(f"\n Total Error is: {total_error}!")
+                acc, pur, nmi, ari = self.evaluator.evaluate_model(y, next_partition)
+                print(f'Finished in Iter: {iter} Cl L: {total_error:.4f} ACC: {acc:.2f} PUR: {pur:.2f} NMI: {nmi:.2f} ARI: {ari:.2f}')
                 return next_partition, total_error
             else:
+                acc, pur, nmi, ari = self.evaluator.evaluate_model(y, next_partition)
+                print(f'Iter: {iter} Cl L: {total_error:.4f} ACC: {acc:.2f} PUR: {pur:.2f} NMI: {nmi:.2f} ARI: {ari:.2f}')
+
                 initial_partition = next_partition
 
     # 6 are good
-    def kernel_kmeans_pp(self, X, K, kernel_matrix, n_init=1, method = 'KkMeans++'):
+    def kernel_kmeans_pp(self, X, y, K, kernel_matrix, n_init=1, method = 'KkMeans++'):
         min_total_error = math.inf
         best_partition = []
         
         for _ in range(n_init):
             clusters_labels, partition = self.initialPartition.calculate_initial_partition(K, X, kernel_matrix, method)
-            partition, total_error = self.custom_kernel_kmeans(X, clusters_labels, initial_partition = partition, kernel_matrix=kernel_matrix, max_iter=300, tol=1e-4)
+            partition, total_error = self.custom_kernel_kmeans(X, y, clusters_labels, initial_partition = partition, kernel_matrix=kernel_matrix, max_iter=300, tol=1e-4)
             
             if(total_error < min_total_error):
                 min_total_error = total_error
