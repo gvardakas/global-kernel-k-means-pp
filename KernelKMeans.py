e1574ebb480239ad82ef5a5559121bc37a443d71
JohnPapakostas <john.s.papakostas@gmail.com>
2024-04-24 18:24:43 +0300

    Update KernelKMeans.py
    1) K to n_clusters
    2) method to init

--- KernelKMeans.py
+++ KernelKMeans.py
@@ -4,21 +4,20 @@ from Initialization import Initialization
 
 class KernelKMeans:
 
-    def __init__(self, K, kernel_matrix, n_init=10, method='k-means++', initial_labels_=None):
+    def __init__(self, n_clusters, kernel_matrix, n_init=10, init='k-means++', initial_labels_=None):
         self.initialization = Initialization()
-        self.K = K
+        self.n_clusters = n_clusters
         self.kernel_matrix = kernel_matrix
         self.n_init = n_init
-        self.method = method
+        self.init = init
         self.initial_labels_ = initial_labels_
     
-
     def find_cluster_indices(self, labels_, cluster_label):
         return np.where(labels_ == cluster_label)[0]
 
     def kernel_kmeans_functionallity(self, N, initial_labels_, kernel_matrix):
-        clusters_identities, initial_labels_ = self.initialization.scale_partition(self.K, initial_labels_)
-        distances = np.zeros((self.K, N))
+        clusters_identities, initial_labels_ = self.initialization.scale_partition(self.n_clusters, initial_labels_)
+        distances = np.zeros((self.n_clusters, N))
         iter = 0
         previous_labels_ = initial_labels_
             
@@ -29,8 +28,8 @@ class KernelKMeans:
 
             iter = 0
             while True:
-                distances = np.zeros((self.K, N))
-                for i in range(self.K):
+                distances = np.zeros((self.n_clusters, N))
+                for i in range(self.n_clusters):
                     cluster_indices = self.find_cluster_indices(previous_labels_, clusters_identities[i])
 
                     n_cluster_samples = len(cluster_indices)
@@ -64,7 +63,7 @@ class KernelKMeans:
         
         for _ in range(self.n_init):
             if(self.initial_labels_ is None):    
-                self.initial_labels_ = self.initialization.calculate_initial_partition(self.K, X, self.kernel_matrix, self.method)
+                self.initial_labels_ = self.initialization.calculate_initial_partition(self.n_clusters, X, self.kernel_matrix, self.init)
 
             current_labels_, total_error = self.kernel_kmeans_functionallity(N, initial_labels_ = self.initial_labels_, kernel_matrix=self.kernel_matrix)
             
