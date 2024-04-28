47faf2290da7b3223adb2977fbf71ebcd34c1c3b
JohnPapakostas <john.s.papakostas@gmail.com>
2024-04-28 20:04:41 +0300

    Update GlobalKernelKMeans.py
    Implement GlobalKernelKMeans in gvardakas way

--- GlobalKernelKMeans.py
+++ GlobalKernelKMeans.py
@@ -1,35 +1,134 @@
-import math
-from Initialization import Initialization
+from abc import ABC, abstractmethod
+import numpy as np
+from sklearn.base import BaseEstimator, ClusterMixin, TransformerMixin
 from KernelKMeans import KernelKMeans
+from sklearn.metrics import pairwise_distances
+from sklearn.utils.validation import check_array, check_is_fitted
 
-class GlobalKernelKMeans:
+class _BaseGlobalKernelKMeans(BaseEstimator, ClusterMixin, TransformerMixin, ABC):
+	"""Base class for Global K-Means, Global K-Means++ and future (or past) variants.
 
-    def __init__(self):
-        self.initialization = Initialization()
-        self.kernelKMeans =  KernelKMeans()
+		Parameters
+		----------
+			n_clusters (int) : The number of clusters to form and the number of centroids to generate.
+			tol (float) : Relative tolerance with regards to Frobenius norm of the difference in the cluster centers of two consecutive iterations to declare convergence.
+			verbose (int) : Verbosity mode.
 
-    def global_kernel_kmeans(self, K, X, kernel_matrix, method = 'global'):
-        min_total_error = math.inf
-        best_labels_ = []
-        best_k_labels_ = []
-        total_min_total_errors = []
-        total_best_k_labels_ = []
-        N = X.shape[0]
-        
-        for k in range(2, K+1):
-            print(f"Now Examining K = {k}")
-            min_k_total_error = math.inf
-            
-            for index in range(N):
-                k_total_error, k_labels_ = self.kernelKMeans.kernel_kmeans(X, k, kernel_matrix, 1, method, initial_labels_=best_labels_, index=index)
-                
-                if(k_total_error < min_k_total_error):
-                    min_k_total_error = k_total_error
-                    best_k_labels_ = k_labels_
-            
-            total_min_total_errors.append(min_k_total_error)
-            total_best_k_labels_.append(best_k_labels_)
-            min_total_error = min_k_total_error
-            best_labels_ = best_k_labels_
-            
-        return min_total_error, best_labels_, total_min_total_errors, total_best_k_labels_     
+		Attributes
+		----------
+			n_iter_ (int) : The total number of KMeans iterations.
+			cluster_centers_ (dict) : Dictionary to store cluster centers for each sub-problem k.
+			labels_ (dict) : Dictionary to store cluster labels for each sub-problem k.
+			inertia_ (dict) : Dictionary to store inertia values for each sub-problem k.
+	"""
+	def __init__(self, n_clusters, kernel_matrix, tol, verbose):
+		self.n_clusters = n_clusters
+		self.kernel_matrix = kernel_matrix
+		self.tol = tol
+		self.verbose = verbose
+		
+		self.n_iter_ = 0
+		#self.cluster_centers_ = {} WHAT TO DO WITH THEM? Maybe Remove them
+		self.labels_ = {}
+		self.inertia_ = {}
+
+	@abstractmethod	
+	def fit(self, X, y=None, sample_weight=None):
+		"""Abstract method for fitting the model to the data.
+
+		Parameters
+		----------
+			X (array-like) : Input data.
+			y : Ignored
+			sample_weight : Ignored
+
+		Returns
+		----------
+			self: Fitted estimator.
+		"""
+		return self
+
+	def predict(self, X=None):
+		"""Predict cluster labels for data X with n_clusters clusters.
+
+		Parameters
+		----------
+			X : Ignored. Models should already be fitted.
+
+		Returns
+		----------
+			labels_ (array) : Cluster labels.
+		"""
+		return self.labels_[self.n_clusters]
+
+	def fit_predict(self, X, y=None, sample_weight=None):
+		"""Compute cluster centers and predict cluster index for each sample.
+
+        Convenience method; equivalent to calling fit(X) followed by predict(X).
+
+
+		Parameters
+		----------
+			X (array-like) : Input data.
+
+		Returns
+		----------
+			labels_ (array) : Cluster labels.
+		"""
+		self.fit(X, y, sample_weight)
+		return self.predict(X)
+
+
+class GlobalKernelKMeans(_BaseGlobalKernelKMeans):
+	"""Global K-Means clustering algorithm.
+
+		Parameters:
+			n_clusters (int) : The number of clusters to form and the number of centroids to generate.
+			tol (float) : Relative tolerance with regards to Frobenius norm of the difference in the cluster centers of two consecutive iterations to declare convergence.
+			verbose (int) : Verbosity mode.
+
+		Attributes
+		----------
+			n_iter_ (int) : The total number of KMeans iterations.
+			cluster_centers_ (dict) : Dictionary to store cluster centers for each sub-problem k.
+			labels_ (dict) : Dictionary to store cluster labels for each sub-problem k.
+			inertia_ (dict) : Dictionary to store inertia values for each sub-problem k.
+
+	"""
+	def __init__(self, n_clusters=8, kernel_matrix=None, tol=1e-4, verbose=0):
+		super().__init__(
+			n_clusters=n_clusters,
+			kernel_matrix=kernel_matrix,
+			tol=tol,
+			verbose=verbose,
+		)
+	
+	def fit(self, X, y=None, sample_weight=None):
+		"""Compute the global k-means clustering.
+
+		Parameters
+		----------
+			X (array-like) : Input data.
+			y : Ignored
+			sample_weight : Ignored
+
+		Returns
+		----------
+			self : Fitted estimator.
+		"""
+		check_array(X)
+		for k in range(2, self.n_clusters+1):
+			if self.verbose > 0: 
+				print(f'Solving Kernel {k}-means')
+						
+			self.inertia_[k] = float('inf')
+			for i, xi in enumerate(X): # TODO parallel
+				kernelKMeans = KernelKMeans(n_clusters=k, kernel_matrix=self.kernel_matrix, n_init=1, initial_labels_=None).fit(X)
+				self.n_iter_ += kernelKMeans.n_iter_
+				
+				if kernelKMeans.inertia_ < self.inertia_[k]:
+					#self.cluster_centers_[k] = kernelKMeans.cluster_centers_
+					self.labels_[k] = kernelKMeans.labels_
+					self.inertia_[k] = kernelKMeans.inertia_
+
+		return self
