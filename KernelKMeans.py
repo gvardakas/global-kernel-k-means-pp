0c39f726ff4571e07c2f0fc975f88731bc32aee6
JohnPapakostas <john.s.papakostas@gmail.com>
2024-04-29 23:13:20 +0300

    Update KernelKMeans.py
    1) Fix while true to 1 while true
    2) Remove ifs with sums = 0
    3) Add min_distances as self

--- KernelKMeans.py
+++ KernelKMeans.py
@@ -12,6 +12,25 @@ class KernelKMeans:
         self.init = init
         self.initial_labels_ = initial_labels_
     
+    def calculate_ground_truth_error(self, labels_):
+        clusters_identities, labels_ = self.initialization.scale_partition(self.n_clusters, labels_)
+        N = len(labels_)
+        kernel_diag = np.diag(self.kernel_matrix)
+        distances = np.zeros((self.n_clusters, N))
+        
+        for i in range(self.n_clusters):
+            cluster_indices = self.find_cluster_indices(labels_, clusters_identities[i])
+
+            n_cluster_samples = len(cluster_indices)
+            stable_sum = np.sum(self.kernel_matrix[np.ix_(cluster_indices, cluster_indices)]) / (n_cluster_samples ** 2)
+            sample_sums = np.sum(self.kernel_matrix[:, cluster_indices], axis=1) / n_cluster_samples
+
+            distances[i] = kernel_diag - 2 * sample_sums + stable_sum
+
+        min_distances = np.min(distances, axis=0)
+        
+        return np.sum(min_distances)
+    
     def find_cluster_indices(self, labels_, cluster_label):
         return np.where(labels_ == cluster_label)[0]
 
@@ -19,39 +38,32 @@ class KernelKMeans:
         clusters_identities, initial_labels_ = self.initialization.scale_partition(self.n_clusters, initial_labels_)
         distances = np.zeros((self.n_clusters, N))
         previous_labels_ = initial_labels_
-            
-        while(True):
-
-            # Precompute kernel_matrix[j,j] and store it in a variable
-            kernel_diag = np.diag(kernel_matrix)
-
-            self.n_iter_ = 0
-            while True:
+        
+        kernel_diag = np.diag(kernel_matrix)
+        self.n_iter_ = 0
+        
+        while True:
                 distances = np.zeros((self.n_clusters, N))
                 for i in range(self.n_clusters):
                     cluster_indices = self.find_cluster_indices(previous_labels_, clusters_identities[i])
 
                     n_cluster_samples = len(cluster_indices)
-                    if(n_cluster_samples ==0):
-                        stable_sum = 0
-                        sample_sums = 0
-                    else:
-                        stable_sum = np.sum(kernel_matrix[np.ix_(cluster_indices, cluster_indices)]) / (n_cluster_samples ** 2)
-                        sample_sums = np.sum(kernel_matrix[:, cluster_indices], axis=1) / n_cluster_samples
+                    stable_sum = np.sum(kernel_matrix[np.ix_(cluster_indices, cluster_indices)]) / (n_cluster_samples ** 2)
+                    sample_sums = np.sum(kernel_matrix[:, cluster_indices], axis=1) / n_cluster_samples
 
                     distances[i] = kernel_diag - 2 * sample_sums + stable_sum
 
-                min_distances = np.min(distances, axis=0)
-                inertia_ = np.sum(min_distances)
+                self.min_distances = np.min(distances, axis=0)
+                inertia_ = np.sum(self.min_distances)
 
                 current_labels_ = np.argmin(distances, axis=0)
                 are_equal = np.array_equal(previous_labels_, current_labels_)
 
                 if are_equal:
-                    print(f'Finished in Iter: {self.n_iter_} Cl L: {inertia_:.4f}')
+                    #print(f'Finished in Iter: {self.n_iter_} Cl L: {inertia_:.4f}')
                     return current_labels_, inertia_
 
-                print(f'Iter: {self.n_iter_} Cl L: {inertia_:.4f}')
+                #print(f'Iter: {self.n_iter_} Cl L: {inertia_:.4f}')
                 previous_labels_ = current_labels_
                 self.n_iter_ += 1
 
