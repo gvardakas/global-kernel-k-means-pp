7d9d2a96a810ad063960cc6feb20a0e4919b6e9f
JohnPapakostas <john.s.papakostas@gmail.com>
2024-10-14 17:17:25 +0300

    Update Modification.py
    Add NC and RA problems modify_kernel_matrix

--- Common_Modules/Modification.py
+++ Common_Modules/Modification.py
@@ -39,3 +39,37 @@ class Modification:
 			except LinAlgError:
 				# It throws LinAlgError if matrix is not positive defined
 				t = max(2 * t, b)
+	
+	def modify_kernel_matrix_ra(self, A, b=0.1):
+		# Initialize I = identity matrix and t = 0
+		I = np.identity(A.shape[0])
+		t = 0
+
+		while(True):
+			modified_matrix = np.add(A, t * I)
+			try:
+				# Check for positive defined matrix with cholesky and return it if it is 
+				L = cholesky(modified_matrix)
+				return modified_matrix
+			except LinAlgError:
+				# It throws LinAlgError if matrix is not positive defined
+				t = max(2 * t, b)
+				print(f"RA t:{t}")
+
+	def modify_kernel_matrix_nc(self, A, D, b=0.1):
+		# Initialize I = identity matrix, K and t = 0
+		D_inv = np.linalg.inv(D)
+		K = D_inv @ A @ D_inv
+		t = 0
+
+		while(True):
+			modified_matrix = np.add(K, t * D_inv)
+			try:
+				# Check for positive defined matrix with cholesky and return it if it is 
+				L = cholesky(modified_matrix)
+				return modified_matrix
+			except LinAlgError:
+				# It throws LinAlgError if matrix is not positive defined
+				t = max(2 * t, b)
+				print(f"NC t:{t}")
+			
